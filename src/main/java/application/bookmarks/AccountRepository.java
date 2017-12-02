b9ba02fa72cf5db55ae413f5ba19a3ee03764e5a
karlklin <pawelski.karol@gmail.com>
2017-12-02 10:46:05 +0100

    javadoc

--- src/main/java/application/bookmarks/AccountRepository.java
+++ src/main/java/application/bookmarks/AccountRepository.java
@@ -8,5 +8,6 @@ import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    // finding user by name
     Optional<Account> findByUsername(String username);
 }
