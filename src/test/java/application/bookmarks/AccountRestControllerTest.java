b51a57700f630df5adbd8f87e13633ea36beba0d
Karol Pawelski <karol.pawelski@cognifide.com>
2017-12-12 14:46:23 +0100

    migrating to JUnit 5 based on: https://github.com/sbrannen/spring-test-junit5

--- src/test/java/application/bookmarks/AccountRestControllerTest.java
+++ src/test/java/application/bookmarks/AccountRestControllerTest.java
@@ -1,14 +1,14 @@
 package application.bookmarks;
 
 import application.Application;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -18,7 +18,8 @@ import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Import(MvcTestConfig.class)
 @AutoConfigureMockMvc
@@ -42,7 +43,7 @@ public class AccountRestControllerTest {
     @Autowired
     private AccountRepository accountRepository;
 
-    @Before
+    @BeforeEach
     public void setup() throws Exception {
         this.bookmarkRepository.deleteAllInBatch();
         this.accountRepository.deleteAllInBatch();
